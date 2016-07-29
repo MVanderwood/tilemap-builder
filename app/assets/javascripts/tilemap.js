019d934f1fe46442e8fb2869a8dc91c5b93a295b
MVanderwood <m.b.vanderwood@gmail.com>
2016-07-28 19:14:14 -0500

    Updates tilemap.js file to reduce namespace pollution.

--- app/assets/javascripts/tilemap.js
+++ app/assets/javascripts/tilemap.js
@@ -1,19 +1,22 @@
-var tilemap = {};
+(function() {
+    var tilemap = {};
 
-function refreshTilemap() {
-    tilemap.width = parseInt(document.getElementById('tilemapWidth').value);
-    tilemap.height = parseInt(document.getElementById('tilemapHeight').value);
-    tilemap.insert = document.getElementById('insert-tilemap');
+    function refreshTilemap() {
+        tilemap.width = parseInt(document.getElementById('tilemapWidth').value);
+        tilemap.height = parseInt(document.getElementById('tilemapHeight').value);
+        tilemap.insert = document.getElementById('insert-tilemap');
 
-    var tableRow = '<tr>';
-    for (var x = 0; x < tilemap.width; x++) { tableRow += '<td class="tilemap-tile"></td>'; }
-    tableRow += '</tr>';
+        var tableRow = '<tr>';
+        for (var x = 0; x < tilemap.width; x++) { tableRow += '<td class="tilemap-tile"></td>'; }
+        tableRow += '</tr>';
 
-    var table = '<table>';
-    for (var y = 0; y < tilemap.height; y++) { table += tableRow; }
-    table += '</table>';
+        var table = '<table>';
+        for (var y = 0; y < tilemap.height; y++) { table += tableRow; }
+        table += '</table>';
 
-    tilemap.insert.innerHTML = table;
-}
+        tilemap.insert.innerHTML = table;
+    }
 
-window.onload = function() { refreshTilemap(); };
+    window.refreshTilemap = refreshTilemap;
+    window.onload = function() { refreshTilemap(); };
+})();
